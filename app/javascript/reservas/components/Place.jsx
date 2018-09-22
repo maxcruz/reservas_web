44ad05e7a236d99fa79c384326b4d8653dace9e9
Max Cruz <max.raul@gmail.com>
2018-09-22 16:44:58 -0500

    Integrate MaterialUI library

--- app/javascript/reservas/components/Place.jsx
+++ app/javascript/reservas/components/Place.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import queryString from 'query-string';
-import axios from 'axios';
+//import { Link } from 'react-router-dom';
+//import queryString from 'query-string';
+//import axios from 'axios';
+import Button from '@material-ui/core/Button';
 
 class Place extends React.Component {
     
     render() {
-        return (<h1>Place</h1>);
+        return (
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        );
     }
 }
 
--- app/javascript/reservas/index.js
+++ app/javascript/reservas/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log("HEREEEE");
-
 const reservas = document.querySelector('#reservas');
 ReactDOM.render(<App />, reservas);
