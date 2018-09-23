7e2988297b504e6f444f4483247a03f4dd796d1c
Max Cruz <max.raul@gmail.com>
2018-09-23 02:10:46 -0500

    Update routes for Places and Sign In

--- app/javascript/reservas/components/App.jsx
+++ app/javascript/reservas/components/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+
 import Place from './Place';
+import SignIn from './SignIn';
 
 const App = (props) => (
   <Router>
 	<div>
 	  <Route
+        exact
 	    path='/'
 	    component={Place}
+	  />
+      <Route
+        exact
+	    path='/login'
+	    component={SignIn}
 	  />
 	</div>
   </Router>
