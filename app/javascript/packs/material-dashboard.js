96e19080c4a194ec2df2ed228c4d0ffe121af627
Max Cruz <max.cruz@getpreo.com>
2018-09-24 18:00:12 -0500

    Populate test data

--- app/javascript/packs/material-dashboard.js
+++ app/javascript/packs/material-dashboard.js
@@ -1,1 +1,1 @@
-import 'material-dashboard';
+
--- app/javascript/reservas/components/Place.jsx
+++ app/javascript/reservas/components/Place.jsx
@@ -71,7 +71,7 @@ const styles = theme => ({
   },
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const cards = [1, 2, 3, 4, 5, 6];
 
 class Place extends React.Component {
 
@@ -95,7 +95,7 @@ class Place extends React.Component {
               variant="flat"
               color="inherit"
               href="/login">
-              Login
+              Ingresar
             </Button>
           </Toolbar>
         </AppBar>
@@ -108,14 +108,14 @@ class Place extends React.Component {
                 align="center"
                 color="textPrimary"
                 gutterBottom>
-                Nombre Lugar
+                La 10 Indoor Soccer
               </Typography>
               <Typography
                 variant="title"
                 align="center"
                 color="textSecondary"
                 paragraph>
-                Una pequeña descripción del lugar junto con la dirección.
+                Calle 108 # 17 A-30 Bogotá, Colombia
               </Typography>
               <div>
                 <Grid container spacing={0}>
@@ -125,8 +125,8 @@ class Place extends React.Component {
                         <PhoneIcon />
                       </ListItemIcon>
                       <ListItemText
-                        primary="123-456-7890"
-                        secondary="Phone"
+                        primary="(57) 310 294 2527"
+                        secondary="Teléfono"
                         />
                     </ListItem>
                   </Grid>
@@ -136,8 +136,8 @@ class Place extends React.Component {
                         <EmailIcon />
                       </ListItemIcon>
                       <ListItemText
-                        primary="foo@foo.com"
-                        secondary="Email"
+                        primary="fiestas@la10indoor.com"
+                        secondary="Correo"
                         />
                     </ListItem>
                   </Grid>
@@ -169,7 +169,7 @@ class Place extends React.Component {
                   <Card className={classes.card}>
                     <CardMedia
                       className={classes.cardMedia}
-                      image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+                      image="http://localhost:3000/indoor-soccer.jpg"
                       // eslint-disable-line max-len
                       title="Image title"
                       />
@@ -178,13 +178,13 @@ class Place extends React.Component {
                         gutterBottom
                         variant="headline"
                         component="h2">
-                        Cancha X
+                        Cancha {card}
                       </Typography>
                       <Typography>
-                        Tamaño: X
+                        Tamaño: 9
                       </Typography>
                       <Typography>
-                        Techada: X
+                        Techada: Si
                       </Typography>
                     </CardContent>
                     <CardActions>
