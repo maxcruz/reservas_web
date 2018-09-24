b4a4d5f12343ff78dbea0aefd62ded20fa51c123
Max Cruz <max.raul@gmail.com>
2018-09-24 09:31:52 -0500

    Fix button style path

--- app/javascript/material-dashboard/assets/jss/material-dashboard-pro-react/components/buttonStyle.jsx
+++ app/javascript/material-dashboard/assets/jss/material-dashboard-pro-react/components/buttonStyle.jsx
@@ -10,7 +10,7 @@ import {
   successColor,
   warningColor,
   dangerColor
-} from "assets/jss/material-dashboard-pro-react.jsx";
+} from "material-dashboard/assets/jss/material-dashboard-pro-react.jsx";
 
 const buttonStyle = {
   button: {
