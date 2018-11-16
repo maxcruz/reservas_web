188f5000cc946cad6ac6231c7d76fea1aba2fedd
Max Cruz <max.raul@gmail.com>
2018-11-16 12:43:58 -0500

    New route for new token request

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope :api do
       get "/place", to: "places#index"
-      get '/field/:id' => "fields#find"
-      get '/field/:id/promos' => "promos#promos"
-      get '/field/:id/events' => "events#events"
-      post '/login' => "sessions#create"
-      get '/logout' => "sessions#destroy"
-      post '/event/checkout' => "events#checkout"
+      get "/field/:id" => "fields#find"
+      get "/field/:id/promos" => "promos#promos"
+      get "/field/:id/events" => "events#events"
+      post "/login" => "sessions#create"
+      get "/logout" => "sessions#destroy"
+      post "/event/checkout" => "events#checkout"
+      get "/event/new_token" => "events#new_token"
   end
 
   root to: "pages#home"
